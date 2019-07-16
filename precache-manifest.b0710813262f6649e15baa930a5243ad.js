9e6366a07f7e5d08ac8b8b124e4439a842cb7e59
Calamatitum <ponomaryovalena@mail.ru>
2019-07-16 22:58:23 +0500

    GitHub Pages

---
+++ precache-manifest.b0710813262f6649e15baa930a5243ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c6121c20e1ceda1d8932",
+    "url": "/static/js/main.6561c2aa.chunk.js"
+  },
+  {
+    "revision": "a40dd20ede2cb907c789",
+    "url": "/static/js/2.b0c06bc5.chunk.js"
+  },
+  {
+    "revision": "c6121c20e1ceda1d8932",
+    "url": "/static/css/main.08ec3191.chunk.css"
+  },
+  {
+    "revision": "a40dd20ede2cb907c789",
+    "url": "/static/css/2.75c849d4.chunk.css"
+  },
+  {
+    "revision": "2e4e6af551f2baf7320155882f536735",
+    "url": "/index.html"
+  }
+];
